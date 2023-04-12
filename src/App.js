fc817c3ef483520aa48797987af501844c26474d
D-Dinara <dinara.dvortsova@gmail.com>
2023-04-12 11:55:32 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import video from './cover-video.mp4'
 
 function App() {
+  const [activity,setActivity] = useState('');
+
+  useEffect(() => {
+    getActivity ();
+}, [])
+
+  const getActivity = async () => {
+  const response = await fetch (`http://www.boredapi.com/api/activity/`);
+  const data = await response.json();
+  setActivity(data.activity);
+} 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video muted autoPlay loop>
+        <source src={video} type="video/mp4"/>
+      </video>
+      <div className='container'>
+        <h1>Get some ideas for activities if you are bored!</h1>
+        
+        <div className='activity-container'>
+          <p className='text'>Try this: </p>
+          <p className='activity'>{activity}</p>
+          <p className='text'>or</p>
+          <button onClick={getActivity}>Click here to get a new activity!</button>
+        </div>
+      </div>
     </div>
   );
 }
